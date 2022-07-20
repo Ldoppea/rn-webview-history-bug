75e5706a159dbb474883ff44df63a1f1a512001e
Yannick Chiron <yannick.chiron@gmail.com>
2022-07-20 14:57:34 +0200

    feat: Add reproduction sample

--- App.js
+++ App.js
@@ -8,104 +8,70 @@
 
 import React from 'react';
 import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import {WebView} from 'react-native-webview';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>A</title>
+  </head>
+  <body>
+    <h1>A</h1>
+    <p><a href="./a.html">link to a</a></p>
+    <p><a href="./b.html">link to b</a></p>
+    <button onclick="window.history.back()">back</button>
+    <button onclick="window.history.forward()">forward</button>
+    <button onclick="push()">push a#n</button>
+    <button onclick="push2()">push b#n</button>
+    <button onclick="push3()">push b</button>
+    <button onclick="locate()">location = b</button>
+    <script>
+      console.log("onload a");
+      window.start = 0;
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+      function push() {
+        const n = ++window.start;
+        window.history.pushState({ n }, \`A \${n}\`, \`./a.html#\${n}\`);
+      }
+
+      function push2() {
+        const n = ++window.start;
+        window.history.pushState({ n }, \`A \${n}\`, \`./b.html#\${n}\`);
+      }
+
+      function push3() {
+        window.history.pushState({}, \`B\`, \`./b.html\`);
+      }
+
+      function locate() {
+        window.location = "./b.html";
+      }
+    </script>
+  </body>
+</html>
+`;
+
+const realSource = {uri: 'https://www.swansontec.com/temp/history/a.html'};
+const localSource = {
+  html: html,
+  baseUrl: 'https://www.swansontec.com/temp/history/a.html',
 };
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.container}>
+      <WebView source={localSource} />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  container: {
+    flex: 1,
   },
 });
 
